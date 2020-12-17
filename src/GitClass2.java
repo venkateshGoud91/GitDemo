0f9b1c05067b37a11a8c190c2171ded8fd48c209
SP <SP@DESKTOP-NFP09IL>
2020-12-17 21:01:38 +0530

    Second Commit

---
+++ src/GitClass2.java
@@ -0,0 +1,16 @@
+
+public class GitClass2 {
+
+	public static void main(String[] args) {
+	
+		String driver = "Yes";
+		if(driver=="Yes")
+		{
+			System.out.println("You can Drive");
+		}else
+		System.out.println("You Cannot Drive");
+			
+
+	}
+
+}
